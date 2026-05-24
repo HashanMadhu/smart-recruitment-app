951d8c000f951857d60c8f4987fb306417461b31
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2026-05-25 01:08:41 +0530

    feat: create mongoose schema and model for user

---
+++ server/models/User.js
@@ -0,0 +1,37 @@
+// server/models/User.js
+// [Language: JavaScript / Node.js]
+
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Please add a name'],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Please add an email'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please add a valid email']
+    },
+    password: {
+        type: String,
+        required: [true, 'Please add a password'],
+        minlength: [6, 'Password must be at least 6 characters'],
+        select: false // Security හේතූන් මත API එකකින් දත්ත ගනිද්දී password එක ඉබේම ලැබීම වළක්වයි
+    },
+    role: {
+        type: String,
+        enum: ['candidate', 'recruiter', 'admin'],
+        default: 'candidate' // Register වන කෙනෙක් default සාමාන්‍ය අයදුම්කරුවෙක් වේ
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
