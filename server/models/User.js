2fbb98657e3a06f297d787b99f94c227a2e75d36
Hashan Madhuranga Dissanayaka <141218317+HashanMadhu@users.noreply.github.com>
2026-05-29 00:57:34 +0530

    fix: add return statement in pre-save middleware to prevent double hashing

--- server/models/User.js
+++ server/models/User.js
@@ -35,14 +35,16 @@ const UserSchema = new mongoose.Schema({
     }
 });
 
-UserSchema.pre('save',async function(next){
+
+UserSchema.pre('save',async function(next){ 
     // If the password is not modified, skip hashin
     if(!this.isModified('password')){
-        next();
+        return next(); 
     }
     // Generate a salt and hash the password
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password,salt);
+    next();
 });
 
 
